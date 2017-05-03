500bec89239bc4cdb87c8294808d4b16e609c0ea
Maple MacBook <tom_maple2@Maples-MacBook.local>
2017-05-03 03:37:13 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+/*
+	server.js
+*/
+const express = require( 'express' );
+const hbs = require( 'hbs' );
+const fs = require( 'fs' );
+
+var app = express();
+hbs.registerPartials( __dirname + '/views/partials' );
+app.set( 'view engine', 'hbs' );
+
+//	express "middleware"
+//	- server logging
+app.use( ( request, response, next ) => {
+	var now = new Date().toString();
+	var logEntry = `${now}: ${request.method} ${request.url}`;
+	console.log( logEntry );
+	fs.appendFile( 'server.log', logEntry + '\n', (err) => {
+		if ( err ) {
+			console.log( 'Unable to append to server log' );
+		}
+	});
+	next();
+});
+//	- uncomment to display a maintenance message
+// app.use(( request, response, next ) => {
+// 	response.render( 'maintenance.hbs', {
+// 		pageTitle: 'System Maintenance'
+// 	});
+// });
+
+//	designate a public directory of static pages
+app.use( express.static( __dirname + '/public' ));
+
+hbs.registerHelper( 'getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+// define a root url mapping for a home page
+app.get( '/', ( request, response ) => {
+	// response.send( 'Hello Express' );
+	response.render( 'home.hbs', {
+		pageTitle: 'Home Page',
+		name: 'Tom',
+		likes: [ 'sushi', 'music', 'my wife' ]
+	});
+});
+
+//	About page
+app.get( '/about', ( request, response ) => {
+	response.render( 'about.hbs', {
+		pageTitle: 'About Page'
+	} );
+});
+
+app.get( '/bad', ( request, response ) => {
+	response.send( { errorMessage: 'Unable to handle request' } );
+});
+
+//	start listening
+app.listen( 3000, () => {
+	console.log( 'Server is up on port 3000' );
+} );
